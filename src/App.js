8973d1cd1ef4ab1f1dec66eac1524f9f8ee6cdda
Marie Findlay <mariealicefindlay@gmail.com>
2019-04-27 14:47:14 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getPast24Hrs = async () => {
+  console.log('inside');
+  const result = await fetch('https://api.carbonintensity.org.uk/intensity/2019-04-27T08:30Z/pt24h')
+  const json = await result.json();
+  return json.data;
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      loading: true,
+      data: []
+    };
+  }
+
+  async componentDidMount() {
+    try{
+      const data = await getPast24Hrs();
+      this.setState({data, loading: false})
+    } catch(error) {
+      this.setState(error);
+    }
+  }
+
+  render() {
+    const { error, loading, data } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (loading) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {data.map(halfHour => (
+            <li>
+              {halfHour.from} {halfHour.intensity.actual}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
 }
 
 export default App;
